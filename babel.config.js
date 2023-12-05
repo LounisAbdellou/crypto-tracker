4400a01f94013c6c027e948fa08d51a96bc1f7be
Lounis Abdellou <lounisabdellou.dev@gmail.com>
2023-12-05 11:07:57 +0100

    ESlint/Prettier + Path aliases

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,21 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      ['module-resolver', {
+        root: ['.'],
+        alias: {
+          '@assets': './assets',
+          '@hooks': './core/hooks',
+          '@services': './core/services',
+          '@components': './core/components',
+          '@contexts': './core/contexts',
+          '@interfaces': './core/interfaces'
+        },
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      }],
+    ],
     env: {
       production: {
         plugins: ['react-native-paper/babel'],
